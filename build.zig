410495ceec26d5c6a74f629a48e8daeb8987163d
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-09-10 11:04:56 +0900

    feat(zine-ssg): Init (#1)
    * chore(rust): Delete
    
    * feat(zig): Init
    
    * feat(build.zig): Edit build.zig and build.zig.zon
    
    * feat(page): Init test page
    
    * chore(flake.nix): Delete package

---
+++ build.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) void {
+    zine.website(b, .{
+        .title = "haruki7049's blogs",
+        .host_url = "https://blogs.haruki7049.dev",
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .assets_dir_path = "assets",
+
+        .static_assets = &.{
+            "fonts/UDEVGothicNF-Regular.ttf",
+            "toppage.css"
+        },
+    });
+}
--- treefmt.nix
+++ treefmt.nix
@@ -1,7 +1,6 @@
 {
   projectRootFile = "flake.nix";
-  programs.rustfmt.enable = true;
   programs.nixfmt.enable = true;
-  programs.taplo.enable = true;
+  programs.zig.enable = true;
   programs.actionlint.enable = true;
 }
