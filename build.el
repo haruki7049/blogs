b62a40e05a584502c8f0cf496a6c345ac6ca2f34
haruki7049 <tontonkirikiri@gmail.com>
2025-03-23 15:13:30 +0900

    Update

--- build.el
+++ build.el
@@ -7,8 +7,11 @@
          :publishing-directory "dist"
          :language "ja"
          :section-numbers t
-         :with-title nil
+         :with-title t
          :with-date nil
-         :html-head-include-default-style nil)))
+         :html-head-include-default-style nil
+         :auto-sitemap t
+         :sitemap-filename "index.org"
+         :sitemap-title "Sitemap for Haruki7049's blogs")))
 
 (org-publish-all t)
